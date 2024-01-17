335d89b19dd0bf200db8c10fbc9b100010ea3c85
Nicolas Mayorga <namayorgav@gmail.com>
2024-01-16 20:07:07 -0500

    fixed: Hydration problem.

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,22 +16,14 @@ export const metadata: Metadata = {
   generator: "Next.js",
   applicationName: "GMAP",
   referrer: "origin-when-cross-origin",
-  keywords: [
-    "Next.js",
-    "React",
-    "JavaScript",
-    "TailwindCSS",
-    "GMAP",
-    "Programming",
-    "Competitive Programming",
-  ],
+  keywords: ["Next.js", "Shortening URLs", "URL shortening", "URL"],
   authors: [
     {
       name: "Nicolas Mayorga",
       url: "https://www.linkedin.com/in/nicol%C3%A1s-mayorga-7z0/",
     },
   ],
-  category: "education",
+  category: "service",
 }
 
 export default function RootLayout({
@@ -41,10 +33,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <div className="fixed top-0 -z-10 h-full w-full bg-white">
-        <div className="absolute inset-0 -z-9 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
-        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-primary-500/35 opacity-50 blur-[80px]"></div>
-      </div>
       <body className={onest.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,17 @@ import Hero from "./components/Hero"
 
 const Home = () => {
   return (
-    <main className="px-8 py-10 ">
-      <Header />
-      <Hero />
-      <FormContainer />
-    </main>
+    <>
+      <div className="fixed top-0 -z-10 h-full w-full bg-white">
+        <div className="absolute inset-0 -z-9 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div>
+        <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-primary-500/40 opacity-50 blur-[80px]"></div>
+      </div>
+      <main className="px-8 py-10 ">
+        <Header />
+        <Hero />
+        <FormContainer />
+      </main>
+    </>
   )
 }
 
