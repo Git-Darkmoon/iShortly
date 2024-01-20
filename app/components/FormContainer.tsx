7c910c2af16bfdfaf0b8ceae54dcd7aa53d0d426
Nicolas Mayorga <namayorgav@gmail.com>
2024-01-20 09:39:46 -0500

    chore: show the generated link.

--- app/components/FormContainer.tsx
+++ app/components/FormContainer.tsx
@@ -3,7 +3,7 @@
 import { FormEvent, useRef, useState } from "react"
 
 const FormContainer = () => {
-  const [shortUrl, setShortUrl] = useState()
+  const [shortUrl, setShortUrl] = useState("...")
   const inputRef = useRef<HTMLInputElement>(null)
 
   async function handleSubmit(e: FormEvent<HTMLFormElement>) {
@@ -20,25 +20,30 @@ const FormContainer = () => {
       body: JSON.stringify({ url }),
     })
     const data = await res.json()
-    setShortUrl(data.shortUrl)
+    console.log(data)
+    setShortUrl(data.shorturl)
+    // Reset the form
+    const form = document.getElementById("myForm") as HTMLFormElement
+    form.reset()
   }
 
   return (
     <>
       <form
+        id="myForm"
         onSubmit={handleSubmit}
-        className="w-full mt-12 max-w-[460px] items-center justify-center text-xl mx-auto px-6 py-4 bg-slate-200 shadow-lg rounded-md"
+        className="w-full mt-12 max-w-[760px] items-center justify-center text-xl mx-auto px-6 py-4 bg-slate-200 shadow-lg rounded-md"
       >
-        <div className="flex gap-x-3">
+        <div className="flex flex-col md:flex-row gap-3">
           <input
-            className="bg-white px-5 py-2.5 rounded-md"
-            type="text"
+            className="w-full bg-white px-5 py-2.5 rounded-md"
+            type="url"
             ref={inputRef}
             placeholder="Place your link here..."
           />
           <button
             type="submit"
-            className="bg-darkViolet hover:bg-darkViolet/85 text-slate-50 px-5 py-2.5 rounded-md transition-colors"
+            className="bg-darkViolet hover:bg-darkViolet/85 text-slate-50 px-5 py-2.5 capitalize rounded-md transition-colors"
           >
             short!
           </button>
