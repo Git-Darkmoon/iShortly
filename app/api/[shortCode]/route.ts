5cceb60a10f4d160167d47e6d590943beb51bf7b
Nicolas Mayorga <namayorgav@gmail.com>
2024-01-20 09:39:32 -0500

    feat: REST endpoint working perfectly.

--- app/api/[shortCode]/route.ts
+++ app/api/[shortCode]/route.ts
@@ -1,18 +1,25 @@
 import { connection } from "@/utils/database"
-import { NextResponse } from "next/server"
+import { redirect } from "next/navigation"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET({ params }: any) {
+export async function GET(request: NextRequest, { params }: any) {
   try {
     const singleGetQuery = "SELECT * FROM link WHERE shorturl = $1"
-    const singleQueryValue = params.shortCode
+    const singleQueryValue = [params.shortCode]
     const response = await connection.query(singleGetQuery, singleQueryValue)
 
-    console.log(response)
-  } catch (error) {
+    if (response.rowCount === 0) {
+      return redirect("/")
+    }
+
+    return NextResponse.redirect(new URL(response.rows[0].originalurl))
+
+    // return NextResponse.json(response.rows[0].originalurl)
+  } catch (error: any) {
     console.error(error)
 
     return NextResponse.json({
-      message: `Error fetching link with code: ${params.shortCode}`,
+      message: error.message,
     })
   }
 }
