16b4a27ab0f71a41dfff793cade972fab9a8f722
Nicolas Mayorga <namayorgav@gmail.com>
2024-01-22 14:09:34 -0500

    chore: improve the performance of the 3D model.

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,28 +1,31 @@
 import Image from "next/image"
 import svgBrowsing from "../../public/assets/images/illustrationBrowsing.svg"
+import FormContainer from "./FormContainer"
+import React, { Suspense } from "react"
+
+const Spline = React.lazy(() => import("@splinetool/react-spline"))
 
 const Hero = () => {
   return (
     <div className="w-full flex flex-col gap-12 md:gap-6 md:flex-row-reverse justify-center items-center h-[calc(100vh-7.5rem)]">
-      <Image
+      {/* <Image
         src={svgBrowsing}
         alt="lorem"
         width={200}
         height={100}
         className="w-full max-w-72 md:max-w-96 xl:max-w-[500px]  bg-cover object-cover"
         priority
-      />
+      /> */}
+      <div className="w-full h-full max-w-72 md:max-w-96 xl:max-w-[500px]">
+        <Suspense fallback={<div>Loading...</div>}>
+          <Spline scene="https://prod.spline.design/WbK2pL0Jox494Mkf/scene.splinecode" />
+        </Suspense>
+      </div>
       <div className="flex flex-col gap-3 items-center md:items-start">
-        <h1 className="text-center md:text-left text-4xl md:max-w-[15ch] md:text-7xl font-extrabold text-slate-900">
+        <h1 className="text-center md:text-left text-4xl md:max-w-[15ch] md:text-7xl font font-extrabold text-slate-200">
           More than just shorter links
         </h1>
-        <p className="md:max-w-[35ch] text-lg text-slate-500/80 font-medium text-center md:text-left text-pretty">
-          Build your brand&apos;s recognition and get detailed insights on how
-          your links are performing
-        </p>
-        <button className="text-xl text-white bg-primary-500 hover:bg-primary-700 px-12 py-3 rounded-xl font-semibold transition-colors">
-          Get Started
-        </button>
+        <FormContainer />
       </div>
     </div>
   )
