5ab483ababf7a520f6e66c4e30f69e13da08584d
Nicolas Mayorga <namayorgav@gmail.com>
2024-01-22 19:06:35 -0500

    feat: copy to clipboard.

--- app/components/LinkElement.tsx
+++ app/components/LinkElement.tsx
@@ -1,17 +1,22 @@
-import { useState } from "react"
+import Image from "next/image"
+import { toast } from "sonner"
+import copyIcon from "../../public/assets/copyIcon.svg"
+
+function handleClick(shortUrl: any) {
+  navigator.clipboard.writeText(`${window.location.host}/api/${shortUrl}`)
+  toast.success("Link copied!")
+}
 
 const LinkElement = ({ shortUrl }: any) => {
-  const [isCopied, setIsCopied] = useState(false)
-
   return (
-    <div className="mt-3 w-full flex justify-between">
-      <div className="mt-4 text-slate-100">{`${window.location.href}api/${shortUrl}`}</div>
+    <div className="mt-3 w-full flex items-center justify-between">
+      <div className="text-sm sm:text-base text-slate-100">{`${window.location.host}/api/${shortUrl}`}</div>
       <button
         type="button"
-        onClick={() => setIsCopied(!isCopied)}
-        className="bg-primary-500 hover:bg-primary-700 text-gray-950 px-3 py-0.5 capitalize rounded-md transition-colors"
+        onClick={() => handleClick(shortUrl)}
+        className="bg-primary-500/75 hover:bg-primary-700 p-2 capitalize rounded-md transition-colors"
       >
-        {isCopied ? "Copied !" : "Copy"}
+        <Image src={copyIcon} alt="copy to clipboard icon" className="size-5" />
       </button>
     </div>
   )
